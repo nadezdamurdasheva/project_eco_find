f5a90d6d160d7255edf5a4b5680bb2eb421f2d2f
nadezdamurdasheva <nadezdamurdasheva@yandex.ru>
2020-11-25 14:28:33 +0300

    adding associations between detailvc and mapvc with segment control.need fix not removing annotations of choosed segment controls

--- project_eco_find/DetailViewController.swift
+++ project_eco_find/DetailViewController.swift
@@ -52,7 +52,24 @@ class DetailViewController: UIViewController {
     @IBAction func onSearchMap(_ sender: UIButton) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         guard let secondViewController = storyboard.instantiateViewController(identifier: "MapViewController") as? MapViewController else { return }
-        secondViewController.index = 0
+        
+        
+        switch image {
+        case UIImage(named: "sign1"),UIImage(named: "sign2"),UIImage(named: "sign3"),UIImage(named: "sign4"),UIImage(named: "sign5"),UIImage(named: "sign6"),UIImage(named: "sign7"),UIImage(named: "sign9"):
+            secondViewController.index = 1
+        case UIImage(named: "sign20"),UIImage(named: "sign21"),UIImage(named: "sign22"),UIImage(named: "sign23"):
+            secondViewController.index = 2
+        case UIImage(named: "sign40"),UIImage(named: "sign41"):
+            secondViewController.index = 3
+        case UIImage(named: "sign50"),UIImage(named: "sign51"),UIImage(named: "sign60"),UIImage(named: "sign61"):
+            secondViewController.index = 4
+        case UIImage(named: "sign70"),UIImage(named: "sign71"),UIImage(named: "sign72"),UIImage(named: "sign73"),UIImage(named: "sign74"),UIImage(named: "sign75"),UIImage(named: "sign76"),UIImage(named: "sign77"),UIImage(named: "sign78"),UIImage(named: "sign79"):
+            secondViewController.index = 5
+        case UIImage(named: "sign80"),UIImage(named: "sign81"),UIImage(named: "sign82"),UIImage(named: "sign83"),UIImage(named: "sign84"),UIImage(named: "sign85"),UIImage(named: "sign87"),UIImage(named: "sign90"),UIImage(named: "sign91"),UIImage(named: "sign92"),UIImage(named: "sign95"),UIImage(named: "sign96"),UIImage(named: "sign97"),UIImage(named: "sign98"),UIImage(named: "signTetrapak"):
+            secondViewController.index = 6
+        default:
+            secondViewController.index = 0
+        }
         
         show(secondViewController, sender: nil)
     }
