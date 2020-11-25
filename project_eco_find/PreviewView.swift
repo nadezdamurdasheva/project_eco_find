e9b30404d59154c86d7665101f87473e2238e968
nadezdamurdasheva <nadezdamurdasheva@yandex.ru>
2020-11-26 01:33:05 +0300

    added wwdc recognizing phone number, but it doesn't work

---
+++ project_eco_find/PreviewView.swift
@@ -0,0 +1,43 @@
+//
+//  PreviewView.swift
+//  project_eco_find
+//
+//  Created by Надя Мурдашева on 26.11.2020.
+//  Copyright © 2020 Надя Мурдашева. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PreviewView: UIView {
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+        guard let layer = layer as? AVCaptureVideoPreviewLayer else {
+            fatalError("Expected `AVCaptureVideoPreviewLayer` type for layer. Check PreviewView.layerClass implementation.")
+        }
+        
+        return layer
+    }
+    
+    var session: AVCaptureSession? {
+        get {
+            return videoPreviewLayer.session
+        }
+        set {
+            videoPreviewLayer.session = newValue
+        }
+    }
+    
+    // MARK: UIView
+    
+    override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
