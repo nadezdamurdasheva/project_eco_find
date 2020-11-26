7fd42d7e0860f91848da1be5b28c397a34ec1e38
nadezdamurdasheva <nadezdamurdasheva@yandex.ru>
2020-11-26 16:43:43 +0300

    created menu for states, games,gamemenu and navigation between them

---
+++ project_eco_find/GameAndStatesMenuViewController.swift
@@ -0,0 +1,32 @@
+//
+//  GameAndStatesMenuViewController.swift
+//  project_eco_find
+//
+//  Created by Надя Мурдашева on 26.11.2020.
+//  Copyright © 2020 Надя Мурдашева. All rights reserved.
+//
+
+import UIKit
+
+class GameAndStatesMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func unwindToGameAndStatesMenuWindow(segue: UIStoryboardSegue){
+        
+    }
+}
